cf3ebc8296656056a4a58761c568f3fac1481fb6
Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
2021-06-27 11:58:23 -0500

    Arch install script mostly working

--- archInstall.sh
+++ archInstall.sh
@@ -10,3 +10,20 @@ while IFS= read -r line; do
     sudo pacman -S $line --noconfirm
   fi
 done < archDeps.txt
+
+# create arrays with list vars
+dotList=($(find $(pwd) -iname '.*' -a ! -iname '.git'))
+baseFileList=($(find $(pwd) -iname '.*' -a ! -iname '.git' -printf "%f\n"))
+
+for i in "${!dotList[@]}"; do
+  printf 'Dotfile %s found, file %s\n' "${dotList[i]}" "${baseFileList[i]}"
+  ln -sv "${dotList[i]}" ~/"${baseFileList[i]}"
+done
+
+# install vundle and plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
+
+# install YCM
+python3 ~/.vim/bundle/youcompleteme/install.py --clangd-completer --cs-completer --go-completer --ts-completer --java-completer
